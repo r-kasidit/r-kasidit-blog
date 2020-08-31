e2546d3a00f9ffb173a03ab8d822775c7d76f2c3
KasiditRison <r.kasidit@outlook.com>
2020-08-31 22:15:39 +0700

    add fb AppID

--- src/components/fbcomment/index.jsx
+++ src/components/fbcomment/index.jsx
@@ -4,7 +4,7 @@ import { FacebookProvider, Comments } from 'react-facebook'
 export default class Fbcomment extends Component {
   render() {
     return (
-      <FacebookProvider appId="123456789">
+      <FacebookProvider appId="234272844571247">
         <Comments href="http://www.facebook.com" />
       </FacebookProvider>
     )
--- src/components/fbshare/index.jsx
+++ src/components/fbshare/index.jsx
@@ -4,7 +4,7 @@ import { FacebookProvider, ShareButton } from 'react-facebook'
 export default class Fbshare extends Component {
   render() {
     return (
-      <FacebookProvider appId="123456789">
+      <FacebookProvider appId="234272844571247">
         <ShareButton href="http://www.facebook.com">
           แชร์ไปยัง facebook
         </ShareButton>
